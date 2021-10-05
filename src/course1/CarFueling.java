bdf8a666a27ea425557b354d4dd2f0aacd20b0c4
Muhammad <69121442+muhammadihabk@users.noreply.github.com>
2021-10-05 10:37:07 +0200

    CarFueling lecture problem

---
+++ src/course1/CarFueling.java
@@ -0,0 +1,41 @@
+package course1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CarFueling {
+
+	public static void main(String[] args) {
+		CarFueling cf = new CarFueling();
+		cf.printPath();
+	}
+	
+    public ArrayList<Integer> minRefills(int[] points, int tank) {
+        ArrayList<Integer> path = new ArrayList<Integer>();
+        int lastRefill = 0;
+        int i = 0;
+        while(i < points.length) {
+            if(points[i]-lastRefill > tank) {
+            	lastRefill = points[i-1];
+            	path.add(i-1);
+            } else {
+                i++;
+            }
+        }
+        return path;
+    }
+    
+    public void printPath() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Number of gas stations: ");
+        int size = input.nextInt();
+        int[] nums = new int[size];
+        System.out.print("Distances: ");
+        for(int i = 0; i < size; i++) {
+            nums[i] = input.nextInt();
+        }
+        
+        ArrayList<Integer> path = minRefills(nums, 400);
+        System.out.println("Refills at: " + path);
+    }
+}
