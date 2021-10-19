89eea3d4ed5d246a98ad0ac2c047ec1747b79a7d
Muhammad Ihab <69121442+muhammadihabk@users.noreply.github.com>
2021-10-19 08:03:10 +0200

    Fixed a bug

--- src/course1/BinarySearch.java
+++ src/course1/BinarySearch.java
@@ -9,11 +9,13 @@ public class BinarySearch {
 		ls.printBinarySearch();
 	}
 	
+	// returns an index of the key
+	// if it's not found, returns the index where it should be put
 	public int binarySearch(int[] nums, int low, int high, int key) {
 		int mid = (high - low) / 2 + low;
-		if(high == low && nums[mid] > key) {
-			return mid;
-		}
+        if(high < low) {
+            return low - 1;
+        }
 		if(nums[mid] == key) {
 			return mid;
 		} else if(nums[mid] > key) {
@@ -24,15 +26,15 @@ public class BinarySearch {
 	}
 
 	public void printBinarySearch() {
-	Scanner input = new Scanner(System.in);
-	System.out.print("Key: ");
-	int key = input.nextInt();
-	System.out.print("Number of elements: ");
-        int length = input.nextInt();
-        int[] nums = new int[length];
-        for(int i = 0; i < nums.length; i++) {
-            nums[i] = input.nextInt();
-        }
-        System.out.println(binarySearch(nums, 0, nums.length-1, key));
-    }
+		Scanner input = new Scanner(System.in);
+		System.out.print("Key: ");
+		int key = input.nextInt();
+		System.out.print("Number of elements: ");
+		int length = input.nextInt();
+		int[] nums = new int[length];
+		for(int i = 0; i < nums.length; i++) {
+			nums[i] = input.nextInt();
+		}
+		System.out.println(binarySearch(nums, 0, nums.length-1, key));
+	}
 }
